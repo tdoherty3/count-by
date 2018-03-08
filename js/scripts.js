78695a6c8af42823a40b3348c90b4be513289f2b
tdoherty3 <thomas@selfsustain.com>
2018-03-07 19:52:47 -0800

    Create HTML and javascript.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+$('#input form').submit(function(event) {
+   event.preventDefault ();
+    debugger;
+    var bigNum = parseInt($('input#userInput').val());
+    var multipleNum = parseInt($('input#countBy').val());
+    var multiples = [];
+
+    for (var i = multipleNum; i <= bigNum; i += multipleNum) {
+      if (bigNum % i === 0) {
+        multiples.push(i);
+      };
+
+      $('#output').text(multiples);
+    };
+  });
+});
